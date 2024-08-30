5e9e0ffe539902678a93868f96870172f4f8f743
davoodya <davoodya40@gmail.com>
2024-08-30 18:44:38 +0330

    Add route_adder.py module

--- practice/file_finder.py
+++ practice/file_finder.py
@@ -1,9 +1,16 @@
-from colorama import Fore
+from colorama import Fore, Style, Back, init
 import subprocess
 import os
 import glob
+import pyfiglet
+import multiprocessing
 
-class FileSystem():
+from concurrent.futures import ThreadPoolExecutor, as_completed
+from functools import partial
+
+init(autoreset=True)
+
+class FileFinderMultiCore():
     def __init__(self):
         self.systemPartition = []
     
@@ -44,12 +51,48 @@ class FileSystem():
         #print(result.stdout.decode())
         #print(result.stderr.decode())
         
+    def search_directory(self, root, file_ext):
+        return glob.glob(os.path.join(root, f'*.{file_ext}'))
+
+    def file_finder_Drive(self, file_ext, drive):
+        drives = [d + ':' for d in 'ABCDEFGHIJKLMNOPQRSTUVWXYZ' if os.path.exists(d + ':')]
+        systemPartition = drives
+
+        with ThreadPoolExecutor() as executor:
+            futures = []
+            for root, dirs, files in os.walk(drive + ':\\'):
+                futures.append(executor.submit(self.search_directory, root, file_ext))
+            
+            for future in as_completed(futures):
+                for file in future.result():
+                    print(file)
+
+        return systemPartition
+
+    def file_finder_all(self, file_ext):
+        drives = [d + ':' for d in 'ABCDEFGHIJKLMNOPQRSTUVWXYZ' if os.path.exists(d + ':')]
+
+        with ThreadPoolExecutor() as executor:
+            futures = []
+            for drive in drives:
+                print(f"Searching in drive {drive}")
+                for root, dirs, files in os.walk(drive + '\\'):
+                    futures.append(executor.submit(self.search_directory, root, file_ext))
+            
+            for future in as_completed(futures):
+                for file in future.result():
+                    print(file)
+    
+
+class FileFinderSingleCore():
+    def __init__(self):
+        pass
     def file_finder_Drive(self, file_ext, drive):
         # Get all available drives
         drives = [d + ':' for d in 'ABCDEFGHIJKLMNOPQRSTUVWXYZ' if os.path.exists(d + ':')]
         self.systemPartition = drives
         
-        print(f'System All Drives: {drives}')
+        #print(f'System All Drives: {drives}')
         
         for root, dirs, files in os.walk(drive + ':\\'):
             # Use glob to find files with the specific extension
@@ -61,7 +104,7 @@ class FileSystem():
     def file_finder_all(self, file_ext):
         # Get all available drives
         drives = [d + ':' for d in 'ABCDEFGHIJKLMNOPQRSTUVWXYZ' if os.path.exists(d + ':')]
-        print(drives)
+        #print(drives)
         
         for drive in drives:
             print(f"Searching in drive {drive}")
@@ -71,11 +114,97 @@ class FileSystem():
                 for file in glob.glob(os.path.join(root, f'*.{file_ext}')):
                     print(file)
 
-print("All Drives: ")
-os.system('fsutil fsinfo drives')
+class FileFinderVerbose():
+    def __init__(self):
+        pass
+    
+    def search_directory(self, root, file_ext):
+        results = []
+        for file in glob.glob(os.path.join(root, f'*.{file_ext}')):
+            results.append(file)
+        return results
 
-fileSystem = FileSystem()
+    def file_finder_Drive(self, file_ext, drive):
+        drives = [d + ':' for d in 'ABCDEFGHIJKLMNOPQRSTUVWXYZ' if os.path.exists(d + ':')]
+        self.systemPartition = drives
+        print(f'System All Drives: {drives}')
 
-drive = input("Select Drive by Enter drive name, Letter Only: ").upper()
-ext = input("Enter File Extension you want to find (without dot): ")
-fileSystem.file_finder_Drive(file_ext=ext, drive=drive)
+        pool = multiprocessing.Pool()
+        search_func = partial(self.search_directory, file_ext=file_ext)
+        
+        for root, dirs, files in os.walk(drive + ':\\'):
+            results = pool.apply_async(search_func, (root,))
+            for file in results.get():
+                print(file)
+        
+        pool.close()
+        pool.join()
+        return self.systemPartition
+
+    def file_finder_all(self, file_ext):
+        drives = [d + ':' for d in 'ABCDEFGHIJKLMNOPQRSTUVWXYZ' if os.path.exists(d + ':')]
+        print(drives)
+
+        pool = multiprocessing.Pool()
+        search_func = partial(self.search_directory, file_ext=file_ext)
+
+        for drive in drives:
+            print(f"Searching in drive {drive}")
+            for root, dirs, files in os.walk(drive + '\\'):
+                results = pool.apply_async(search_func, (root,))
+                for file in results.get():
+                    print(file)
+
+        pool.close()
+        pool.join()
+
+
+def print_banner():
+    banner = pyfiglet.figlet_format("Yakuza File Engine", font="slant")
+    print(Style.BRIGHT+Fore.BLUE + banner + Style.RESET_ALL)
+  
+def print_menu():
+    print(Fore.CYAN + "=" * 50)
+    print(Fore.YELLOW + "1. Find files in a specific drive")
+    print(Fore.YELLOW + "2. Find files in all drives")
+    print(Fore.YELLOW + "3. List partitions")
+    print(Fore.YELLOW + "4. Exit")
+    print(Fore.CYAN + "=" * 50)
+    
+    print("All Drives: ", end="")
+    os.system('fsutil fsinfo drives')
+
+def main():
+    fileFinderMC = FileFinderMultiCore()
+    
+    while True:
+        print_banner()
+        print_menu()
+        
+        choice = input(Fore.GREEN + "Enter your choice (1-4): " + Fore.RESET)
+        
+        if choice == '1':
+            drive = input(Fore.MAGENTA + "Enter drive letter: " + Fore.RESET).upper()
+            ext = input(Fore.MAGENTA + "Enter file extension (without dot): " + Fore.RESET)
+            print(Fore.CYAN + "\nSearching files...\n")
+            fileFinderMC.file_finder_Drive(file_ext=ext, drive=drive)
+        elif choice == '2':
+            ext = input(Fore.MAGENTA + "Enter file extension (without dot): " + Fore.RESET)
+            print(Fore.CYAN + "\nSearching files in all drives...\n")
+            fileFinderMC.file_finder_all(file_ext=ext)
+        elif choice == '3':
+            print(Fore.CYAN + "\nListing partitions...\n")
+            fileFinderMC.list_partitions()
+        elif choice == '4':
+            print(Fore.RED + "\nExiting Yakuza File Engine. Goodbye!")
+            break
+        else:
+            print(Fore.RED + "\nInvalid choice. Please try again.")
+        
+        input(Fore.GREEN + "\nPress Enter to continue..." + Fore.RESET)
+        os.system('cls' if os.name == 'nt' else 'clear')
+
+if __name__ == "__main__":
+    main()  
+
+
--- practice/res.py
+++ practice/res.py
@@ -1,24 +1,7 @@
-import os
-import glob
+from win10toast import ToastNotifier
 
-def file_finder(file_ext):
-    # Get all available drives
-    drives = [d + ':' for d in 'ABCDEFGHIJKLMNOPQRSTUVWXYZ' if os.path.exists(d + ':')]
-    print(drives)
-    drive = input("Enter drive name, Letter Only: ")
-    for root, dirs, files in os.walk(drive + ':\\'):
-        # Use glob to find files with the specific extension
-        for file in glob.glob(os.path.join(root, f'*.{file_ext}')):
-            print(file)
-    
-    
-    # for drive in drives:
-    #     print(f"Searching in drive {drive}")
-    #     # Use os.walk to iterate through all directories
-    #     for root, dirs, files in os.walk(drive + '\\'):
-    #         # Use glob to find files with the specific extension
-    #         for file in glob.glob(os.path.join(root, f'*.{file_ext}')):
-    #             print(file)
+toast = ToastNotifier()
 
-fileExt = input('Enter File Extension you want to find (without dot): ')
-file_finder(fileExt)
+toast.show_toast("Tele-RAT","Hello Ninja Please keep your connection open", duration=10)
+
+#toast.notification_active()
